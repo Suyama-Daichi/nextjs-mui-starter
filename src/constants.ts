49264aae735302a89370e8202182b24a5f68436e
daichi.suyama <symdit@gmail.com>
2022-12-19 17:13:22 +0900

    fix

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 export const literals = {
-    brand: 'HC Cloud(開発版)',
-    defaultPageDescription: 'HC Cloud(開発版)',
+    brand: 'サービス名',
+    defaultPageDescription: 'サービス名',
 }
