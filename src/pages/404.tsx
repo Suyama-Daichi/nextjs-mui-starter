d8f21a9bc5d313f7d9ced973150ba18d64a751ac
daichi.suyama <symdit@gmail.com>
2022-12-26 17:02:37 +0900

    メタデータを付与

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
 const Custom404 = () => {
-    return <h1>404 - Page Not Found</h1>
+    return (
+        <>
+            <Head>
+                <title>ページが見つかりません</title>
+                <meta name="description" content={'ページが見つかりません'} />
+            </Head>
+            <h1>404 - Page Not Found</h1>
+        </>
+    )
 }
 
 export default Custom404
